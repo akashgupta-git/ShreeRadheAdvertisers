65ffbdf30a1573d50cbd3d4c667afec2c078b646
Akash Gupta <akashgupta.20044@gmail.com>
2025-12-26 15:11:57 +0530

    Commit Backend allow PATCH requests

--- sra-backend/server.js
+++ sra-backend/server.js
@@ -53,7 +53,7 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(morgan('combined'));
